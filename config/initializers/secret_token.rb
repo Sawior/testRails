3ff08a326792c8d6f7bff2ad53a7aff3a622e2cd
Sawior <sawio@poczta.onet.pl>
2013-04-04 12:39:12 +0200

    Wersja ze zdjęciami

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlogYT::Application.config.secret_token = '8d031a9e37b7becb06b2c8aface7da1aafa636806e99de7bf4391bc3164297e020ccef72f111d612ec1cc9564a394a85b828309f9d0c6a472412ecff9676e046'
